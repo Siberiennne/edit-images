897b99e32131ee1aa6d7e7d38daed9094400bfb8
siberiennne <siberienne94@yandex.ru>
2020-09-15 12:14:15 +0700

    feat: add_service_to_post_image

---
+++ src/app/http.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class HttpService {
+
+    constructor(private http: HttpClient) { }
+
+    postImage(url: string) {
+        const endpoint = 'http://localhost:3000/upload';
+        const formData: FormData = new FormData();
+
+        let blobBin = atob(url.split(',')[1]);
+        let array = [];
+        for (let i = 0; i < blobBin.length; i++) {
+            array.push(blobBin.charCodeAt(i));
+        }
+
+        let blob = new Blob([new Uint8Array(array)], { type: 'image/png' });
+        formData.append('image', blob);
+  
+        return this.http
+            .post(endpoint, formData, {
+                responseType: 'blob', reportProgress: true, observe: "events"
+            })
+
+    }
+}
