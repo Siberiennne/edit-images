f93c1b43b6efd6bb4127df34535ec06e72618409
siberiennne <siberienne94@yandex.ru>
2020-09-15 19:59:15 +0700

    feat: add_img_comp

--- src/imageCanvas/image.component.ts
+++ src/imageCanvas/image.component.ts
@@ -11,30 +11,25 @@ export class ImageComponent implements OnInit, OnChanges {
     @ViewChild('imgCanvas', { static: true })
     imgCanvas: ElementRef<HTMLCanvasElement>;
     imgCtx: CanvasRenderingContext2D;
-
     @Input() imgCoor: ImageCoordinate;
+    @Input() cropCoor: ImageCoordinate;
     @Input() img: HTMLImageElement;
     @Input() imgDataEdited: ImageData;
-    @Output() onMakeGray = new EventEmitter();
+    @Input() applyCrop: boolean;
+    @Input() compressionRatio: number;
+
     @Output() onGetImageData = new EventEmitter<ImageData>();
+    @Output() onSaveCanvas = new EventEmitter<string>();
 
     imgData: ImageData = null;
-    imgSrc: string = "";
+   
 
     ngOnInit() {
         this.imgCtx = this.imgCanvas.nativeElement.getContext('2d');
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        console.log("SimpleChanges");
-        console.log(changes);
-        // if (changes.imgCoor) {
-        //     if (!this.imgCoor.isTrivial()) {
-        //         this.clearImageCanvas();
-        //         this.imgCtx.drawImage(this.img, 0, 0, this.imgCoor.rect.area.w, this.imgCoor.rect.area.h);
-        //         return;
-        //     }
-        // }
+
         if (changes.imgDataEdited) {
             if (changes.imgDataEdited.currentValue) {
                 this.clearImageCanvas();
@@ -42,47 +37,59 @@ export class ImageComponent implements OnInit, OnChanges {
                 return;
             }
         }
-        // if (changes.img && !this.imgCoor.isTrivial()) {
-        //     if (changes.img.currentValue) {
-                this.clearImageCanvas();
-                // this.imgCtx = this.createImageCanvas(this.imgCoor.area);
-                this.createImageCanvas(this.imgCoor)
-                this.imgCtx.drawImage(this.img, 0, 0, this.imgCoor.rect.area.w, this.imgCoor.rect.area.h);
-                this.imgData = this.imgCtx.getImageData(0, 0, this.imgCoor.rect.area.w, this.imgCoor.rect.area.h);
-                this.getImageData();
-           // }
-       // }
+        if (this.imgCtx && this.imgCoor) {
+            if (!this.imgCoor.isTrivial())
+                this.draw();
+        }
+
     }
 
     getImageData() {
         this.onGetImageData.emit(this.imgData);
     }
 
-    draw(ctx: CanvasRenderingContext2D) {
+    getDataURL() {
+        let data: string = this.imgCanvas.nativeElement.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        this.onSaveCanvas.emit(data);
+    }
 
+    draw() {
+        this.clearImageCanvas();
+        if (this.applyCrop && !this.cropCoor.isTrivial()) {
+            this.createImageCanvas(this.cropCoor)
+            let diffLeft = Math.abs(this.imgCoor.startPoint.x - this.cropCoor.startPoint.x)
+            let diffTop = Math.abs(this.imgCoor.startPoint.y - this.cropCoor.startPoint.y)
+            this.imgCtx.drawImage(this.img, diffLeft / this.compressionRatio, diffTop / this.compressionRatio, this.cropCoor.rect.w / this.compressionRatio, this.cropCoor.rect.h / this.compressionRatio, 0, 0, this.cropCoor.rect.w, this.cropCoor.rect.h);
+        }
+        else {
+            this.createImageCanvas(this.imgCoor)
+            this.imgCtx.drawImage(this.img, 0, 0, this.imgCoor.rect.w, this.imgCoor.rect.h);
 
+        }
+
+        this.imgData = this.imgCtx.getImageData(0, 0, this.imgCoor.rect.w, this.imgCoor.rect.h);
+        this.getImageData();
+        this.getDataURL();
     }
 
 
     createImageCanvas(data: ImageCoordinate) {
-        // this.imgCanvas = document.createElement('canvas');
-        // this.imgCanvas.style.display = 'none';
-        // document.body.appendChild(this.imgCanvas);
-        this.imgCanvas.nativeElement.width = data.rect.area.w
-        this.imgCanvas.nativeElement.height = data.rect.area.h;
+        this.imgCanvas.nativeElement.width = data.rect.w
+        this.imgCanvas.nativeElement.height = data.rect.h;
         this.imgCanvas.nativeElement.style.position = 'absolute';
-        this.imgCanvas.nativeElement.style.top = data.startPoint.coor.y + 'px';
-        this.imgCanvas.nativeElement.style.left = data.startPoint.coor.x + 'px';
-        // return this.imgCanvas.nativeElement.getContext('2d');
+        this.imgCanvas.nativeElement.style.top = data.startPoint.y + 'px';
+        this.imgCanvas.nativeElement.style.left = data.startPoint.x + 'px';
     }
 
     clearImageCanvas() {
-        this.imgCtx.clearRect(0, 0, this.imgCanvas.nativeElement.width, this.imgCanvas.nativeElement.height);
+        if (this.imgCtx)
+            this.imgCtx.clearRect(0, 0, this.imgCanvas.nativeElement.width, this.imgCanvas.nativeElement.height);
     }
- 
+
 
     putImageData(imgData: ImageData): void {
-        this.imgCtx.putImageData(imgData, 0, 0);
+        if (this.imgCtx)
+            this.imgCtx.putImageData(imgData, 0, 0);
     }
 
 }
